a2311e8fc7da08e555b97210caa971ab92db5049
Perry <srperry.2017@mymail.jcsu.edu>
2018-01-24 14:58:31 -0500

    first

---
+++ division/Division.java
@@ -0,0 +1,13 @@
+package division;
+
+public class Division {
+
+	public static void main(String[] args) {
+		int quotient = 158/11;
+		double remainder = (158%11);
+		System.out.println("The Quotient is "+ quotient);
+		System.out.println("The Remainder is "+ remainder);
+
+	}
+
+}
